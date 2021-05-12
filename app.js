4a7f2b1951ded3bc68184994dd1dda4989f66f17
envincebal <vincentyan1@gmail.com>
2021-05-12 04:13:11 -0700

    Added User routes.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+// Middleware
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,8 +17,10 @@ app.use((req, res, next) => {
   next();
 });
 
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+// Route handlers
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -94,21 +99,71 @@ const deleteTour = (req, res) => {
   })
 }
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status:"error",
+    message: "This route is not yet defined"
+  })
+}
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status:"error",
+    message: "This route is not yet defined"
+  })
+}
+const createUser = (req, res) => {
+  res.status(500).json({
+    status:"error",
+    message: "This route is not yet defined"
+  })
+}
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status:"error",
+    message: "This route is not yet defined"
+  })
+}
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status:"error",
+    message: "This route is not yet defined"
+  })
+} 
+
 // app.get("/api/v1/tours", getAllTours);
 // app.post("/api/v1/tours", createTour);
 // app.get("/api/v1/tours/:id", getTour);
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
-app.route("/api/v1/tours")
+// Routes
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+tourRouter.route("/")
   .get(getAllTours)
   .post(createTour);
 
-app.route("/api/v1/tours/:id")
+tourRouter.route("/:id")
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
+userRouter.route("/")
+  .get(getAllUsers)
+  .post(createUser)
+
+userRouter.route("/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// Start server
 const port = 3000;
 app.listen(port, () => {
   console.log("App running on port " + port);
