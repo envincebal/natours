a9aa6acd5f52194665ffdcc2106ed5a0d775ec8a
envincebal <vincentyan1@gmail.com>
2021-05-16 19:32:58 -0700

    Added models folder and CRUD operations.

--- server.js
+++ server.js
@@ -15,38 +15,7 @@ mongoose.connect(DB, {
   })
   .then(() => console.log("DB connection successful!"));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "A tour must have a name"],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    require: [true, "A tour must have a price"]
-  },
-});
 
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-  name: "The Park Camper", 
-  rating: 4.7,
-  price: 497
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log("Error:", err);
-  });
 
 // Start server
 const port = process.env.PORT || 3000;
