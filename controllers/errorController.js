141d45ae48f1d2ec646cf0007d8ab2018cfb71d5
envincebal <vincentyan1@gmail.com>
2021-06-11 23:42:00 -0700

    Added login and signin routes.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,3 @@
-
 const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = err => {
@@ -13,7 +12,6 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
-
 const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
