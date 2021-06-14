0bc97e1ec548888d1aa822a33d6ba71ef9ff1fa9
envincebal <vincentyan1@gmail.com>
2021-06-14 00:06:33 -0700

    Added mongoose and CRUD operations.

--- app.js
+++ app.js
@@ -1,109 +1,31 @@
-const fs = require("fs");
 const express = require('express');
-
+const morgan = require("morgan");
 const app = express();
 
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// 1. Middleware
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next();
+});
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  const id = Number(req.params.id);
-  const tour = tours.find(el => el.id === id);
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "invalid ID"
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour
-    }
-  });
-}
-
-const createTour = (req, res) => {
-  // console.log(req,body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({
-    id: newId
-  }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: "Success",
-      data: {
-        tour: newTour
-      }
-    })
-  })
-};
-
-const updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID"
-    })
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "Updated tour here"
-    }
-  })
-}
-
-const deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID"
-    })
-  }
-
-  res.status(204).json({
-    status: "success",
-    data: null
-  })
-};
-
-// app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getTour);
-// app.post("/api/v1/tours", createTour);
-// app.patch("/api/v1/tours/:id", updateTour);
-// app.delete("/api/v1/tours/:id", deleteTour);
-
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour)
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour)
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 })
+
+// 2. Routes
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/tours", userRouter);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "./config.env"
+});
+const app = require("./app");
+
+dotenv.config({
+  path: "./config.env"
+});
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("DB connection successful"));
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+})
