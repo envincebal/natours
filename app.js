9317e9ee53fd6c179d93605590bec5d98cfae5df
envincebal <vincentyan1@gmail.com>
2021-06-18 00:05:32 -0700

    Added error handling.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
 // 1. Middleware
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === "development") {
@@ -15,17 +18,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 })
 
 // 2. Routes
 app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/tours", userRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,28 +1,37 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({
-  path: "./config.env"
+
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
 });
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-dotenv.config({
-  path: "./config.env"
-});
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
-
-// mongoose.connect(process.env.DATABASE_LOCAL, {
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
     useFindAndModify: false
   })
-  .then(() => console.log("DB connection successful"));
-
-
+  .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-})
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
